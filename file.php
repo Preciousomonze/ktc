7dc083b2402fc43f06b4715d9398a3d2f27b2c59
Precious Omonzejele <crispusomonze@gmail.com>
2019-05-11 15:46:17 +0100

    Create file.php

---
+++ file.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Loystar for Woocommerce
+ * Plugin URI: https://loystar.co/wordpress
+ * Description: Connect your Loystar account with your Woo-Commerce store and have all your transaction, customer and loyalty records in one place. Truly omni-channel.
+ * Author: Loystar Solutions
+ * Author URI: http://loystar.co
+ * Version: 2.0.0
+ * Requires at least: 4.9.0
+ * Tested up to: 5.2.0
+ * WC requires at least: 3.0
+ * WC tested up to: 3.6
+ * 
+ * Text Domain: loystar-woocommerce-loyalty-program
+ *
+ */
